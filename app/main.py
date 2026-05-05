0819e891f69f0fb732d8dd7e868a1ec9779fc30d
Matas Stankevičius <m.stankevicius@student.tudelft.nl>
2026-05-05 11:10:44 +0200

    Basic frontend for app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routers import uploads
 from app.database import Base, engine
 from app import models
-
+from app.routers import pages
 
 Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(uploads.router)
+app.include_router(pages.router)
